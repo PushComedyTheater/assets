74d1ea04795091b87fea0ec7a908b39281a259cc
Patrick Veverka <patrick.veverka@gmail.com>
2018-01-09 22:32:22 -0500

    assets

---
+++ javascripts/app.js
@@ -0,0 +1,81 @@
+/*   
+ * Template Name: Unify - Responsive Bootstrap Template
+ * Description: Business, Corporate, Portfolio and Blog Theme.
+ * Version: 1.3
+ * Author: Html Stream
+ * Website: http://htmlstream.com/preview/unify
+*/
+
+var App = function () {
+
+    function handleIEFixes() {
+        //fix html5 placeholder attribute for ie7 & ie8
+        if (jQuery.browser.msie && jQuery.browser.version.substr(0, 1) < 9) { // ie7&ie8
+            jQuery('input[placeholder], textarea[placeholder]').each(function () {
+                var input = jQuery(this);
+
+                jQuery(input).val(input.attr('placeholder'));
+
+                jQuery(input).focus(function () {
+                    if (input.val() == input.attr('placeholder')) {
+                        input.val('');
+                    }
+                });
+
+                jQuery(input).blur(function () {
+                    if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                        input.val(input.attr('placeholder'));
+                    }
+                });
+            });
+        }
+    }
+
+    function handleBootstrap() {
+        jQuery('.carousel').carousel({
+            interval: 15000,
+            pause: 'hover'
+        });
+        jQuery('.tooltips').tooltip();
+        jQuery('.popovers').popover();
+    }
+
+    function handleSearch() {    
+        jQuery('.search').click(function () {
+            if(jQuery('.search-btn').hasClass('icon-search')){
+                jQuery('.search-open').fadeIn(500);
+                jQuery('.search-btn').removeClass('icon-search');
+                jQuery('.search-btn').addClass('icon-remove');
+            } else {
+                jQuery('.search-open').fadeOut(500);
+                jQuery('.search-btn').addClass('icon-search');
+                jQuery('.search-btn').removeClass('icon-remove');
+            }   
+        }); 
+    }
+
+
+    function handleBoxed() {
+        jQuery('.boxed-layout-btn').click(function(){
+            jQuery(this).addClass("active-switcher-btn");
+            jQuery(".wide-layout-btn").removeClass("active-switcher-btn");
+            jQuery("body").addClass("boxed-layout container");
+        });
+        jQuery('.wide-layout-btn').click(function(){
+            jQuery(this).addClass("active-switcher-btn");
+            jQuery(".boxed-layout-btn").removeClass("active-switcher-btn");
+            jQuery("body").removeClass("boxed-layout container");
+        });
+    }
+
+    return {
+        init: function () {
+            handleBootstrap();
+            handleIEFixes();
+            handleSearch();
+            handleBoxed();
+        },
+
+    };
+
+}();
